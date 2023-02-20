be1f094ae786fa8692c717f33fa569436de429aa
gwangjinjeong <git.gwangjin@gmail.com>
2023-02-21 08:18:50 +0900

    Today I Learned - 처음 시도 : 누적합 요소 중에서 combination 라이브러리를 통해서 2개를 뽑아서 빼고 % M이 0 이면 cnt+1 ==> 메모리 초과 - 두번째 시도 성공!   - 누적합의 % 3을 미리 적용 시켜서 요소별로 Combination을 했다는 점과   - Counter를 통해서 메모리 효율을 높인게 크지 않나 싶다.

--- boj/10986_remain-sum.py
+++ boj/10986_remain-sum.py
@@ -1,12 +1,29 @@
-from itertools import combinations
 from itertools import accumulate
+from collections import Counter
 import sys
+
+def sum_combination(n,r):
+    data = 1
+    for i in range(r):
+        data = data * (n-i)/(r-i)
+    return int(data)
 
 si = sys.stdin.readline
 
 N, M = list(map(int,si().split()))
 A = list(map(int,si().split()))
-S = [0] + list(accumulate(A))
-S_combination = list(combinations(S, 2))
-result = list(map(lambda x : 1 if (x[1] - x[0]) % M == 0 else 0, S_combination))
-print(sum(result))
+# 누적합에 %3해서 0~i까지 그대로 떨어지는 경우의 수 구하기
+S = list(accumulate(A))
+S3 = list(map(lambda a: a % M, S))
+
+# Counter를 통해서 요소별로 갯수를 구하기
+count_S = Counter(S3)
+
+# key가 0인것은 구간합(0~i)까지가 이미 나누어 떨어진다는것 이것을 초기값으로 두자
+cnt = count_S[0]
+
+# 그 밖에 구간합(i~j)가 나누어 떨어지는것을 확인하려면, 2개뽑아서 뺄 필요없이 조합 공식으로 구하면 된다.
+# 순서에 의미가 있고, 중복이 허용되지 않기 때문에 조합이다.
+for value in count_S.values():
+    cnt = cnt + sum_combination(value,2)
+print(cnt)
